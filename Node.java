3d84689456bef2ef0a03fa8d22c9dc06eb6e6513
Elias Ferguson <eferguson00@stuy.edu>
2018-12-13 11:25:47 -0500

    added more skeleton

--- Node.java
+++ Node.java
@@ -4,4 +4,25 @@ class Node {
  public Node(int num) {
    data = num;
  }
+ private Node next() {
+
+ }
+ private Node prev() {
+
+ }
+ private void setNext(Node other) {
+
+ }
+ private void setPrev(Node other) {
+
+ }
+ public Integer getData() {
+
+ }
+ public Integer setData(Integer i) {
+
+ }
+ public String toString() {
+
+ }
 }
