a6a8a38e655417f5fb97c01a1db09e3ade612a05
Elias Ferguson <eferguson00@stuy.edu>
2018-12-12 19:57:28 -0500

    completed size()

--- LinkedList.java
+++ LinkedList.java
@@ -2,7 +2,13 @@ class MyLinkedList {
  private int size;
  private Node start,end;
 
- public int size();
- public boolean add(int value);
- public String toString();
+ public int size() {
+   return size;
+ }
+ public boolean add(int value) {
+
+ }
+ public String toString() {
+   
+ }
 }
