1b771f96750414c28438651cc168e554310d2f60
Elias Ferguson <eferguson00@stuy.edu>
2018-12-14 11:16:16 -0500

    added private class Node

--- LinkedList.java
+++ LinkedList.java
@@ -7,7 +7,7 @@ class MyLinkedList {
  public int size() {
    return size;
  }
- public boolean add(int value) {
+ public boolean add(Integer value) {
    if (size == 0) {
      start = new Node(value);
      end = start;
@@ -19,6 +19,43 @@ class MyLinkedList {
   return true;
  }
  public String toString() {
-   return "help me";
- }
+   Node current = start;
+   String output = "[";
+   while (current != null) {
+     output += current.toString() + ", ";
+     current = current.next();
+     
+   }
+   return output + "]";
+  }
+  private class Node {
+   private int data;
+   private Node next,prev;
+   private Node(int num) {
+     data = num;
+   }
+   private Node next() {
+     return next;
+   }
+   private Node prev() {
+     return prev;
+   }
+   private void setNext(Node other) {
+     next = other;
+   }
+   private void setPrev(Node other) {
+     prev = other;
+   }
+   private Integer getData() {
+     return data;
+   }
+   private Integer setData(Integer i) {
+    Integer old = data;
+    data = i;
+    return old;
+   }
+   public String toString() {
+     return "" + getData();
+   }
+  }
 }
